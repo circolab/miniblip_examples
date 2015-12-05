c8631bf506c9e4f1d8162268a70661cedaf6c5d1
Pablo Clemente <paclema@gmail.com>
2015-12-05 11:15:30 +0100

    Updated piano example

--- piano_board/piano.py
+++ piano_board/piano.py
@@ -9,7 +9,14 @@ pygame.display.set_mode((120, 120), DOUBLEBUF | HWSURFACE)
 
 pygame.init()
 
-crash_sound = pygame.mixer.Sound('68448__pinkyfinger__piano-g.wav')
+sound1 = pygame.mixer.Sound('sound1.wav')
+sound2 = pygame.mixer.Sound('sound2.mp3')
+sound3 = pygame.mixer.Sound('sound3.mp3')
+sound4 = pygame.mixer.Sound('sound4.mp3')
+sound5 = pygame.mixer.Sound('sound5.wav')
+sound6 = pygame.mixer.Sound('sound6.wav')
+sound7 = pygame.mixer.Sound('sound7.wav')
+sound8 = pygame.mixer.Sound('sound8.wav')
 #pygame.mixer.music.load('68448__pinkyfinger__piano-g.wav')
 
 #pygame.mixer.Sound.play(crash_sound)
@@ -26,7 +33,7 @@ display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('MniBlip sound player')
 
 black = (0,0,0)
 white = (255,255,255)
@@ -52,9 +59,24 @@ while not crashed:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_change = -5
-                pygame.mixer.Sound.play(crash_sound)
+                pygame.mixer.Sound.play(sound1)
             elif event.key == pygame.K_RIGHT:
-                x_change = 5
+				#x_change = + 5
+                pygame.mixer.Sound.play(sound2)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Sound.play(sound3)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Sound.play(sound4)
+            elif event.key == pygame.K_RIGHT:
+				pygame.mixer.Sound.play(sound5)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Sound.play(sound6)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Sound.play(sound7)
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.Sound.play(sound8)
+                
+                
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
